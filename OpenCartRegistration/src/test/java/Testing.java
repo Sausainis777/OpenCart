e69756a18b4634110d6c75c355e1c3d7bd3e564f
Sausainis777 <a.tankeliun@gmail.com>
2023-10-12 08:56:24 +0300

    add method

--- OpenCartRegistration/src/test/java/Testing.java
+++ OpenCartRegistration/src/test/java/Testing.java
@@ -27,6 +27,9 @@ public class Testing {
     void driver() {
 
         driver = new ChromeDriver();
+        driver.get("http://192.168.8.129/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
     }
 
@@ -34,50 +37,60 @@ public class Testing {
     void teardown() {
         driver.quit();
     }
+    public String generateEmail(){
+        String emailAddress = "";
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        while (emailAddress.length() < 5) {
+            int character = (int) (Math.random() * 26);
+            emailAddress += alphabet.substring(character, character + 1);
+            emailAddress += Integer.valueOf((int) (Math.random() * 99)).toString();
+            emailAddress += "@gmail.com" ;}
+        return emailAddress;
 
-    @Disabled
+    }
+
+
     @ParameterizedTest
     @CsvFileSource(files = "src/main/resources/Users.csv", numLinesToSkip = 1)
     void registration(String firstName, String lastName, String email, String password) throws InterruptedException {
 
 
-        driver.get("http://192.168.8.129/");
+        Registration page = new Registration(driver);
+        MainPage mainPage = new MainPage(driver);
 
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-        driver.findElement(By.cssSelector(".list-inline-item:nth-of-type(2) .d-md-inline")).click();
-        driver.findElement(By.linkText("Register")).click();
 
-        driver.findElement(By.id("input-firstname")).sendKeys(firstName);
-        driver.findElement(By.id("input-lastname")).sendKeys(lastName);
-        driver.findElement(By.id("input-email")).sendKeys(email);
-        driver.findElement(By.id("input-password")).sendKeys(password);
+        mainPage.clickMyAccount();
+        mainPage.clickRegister();
 
-        driver.findElement(By.cssSelector("input[name='agree']")).click();
-        driver.findElement(By.cssSelector(".btn-primary")).click();
+        page.setFirstName(firstName);
+        page.setLastName(lastName);
+        page.setEmail(generateEmail());
+        page.setPassword(password);
+        page.agreeButtonClick();
+        page.registerButtonClick();
+
         Thread.sleep(2000);
-
 
         assertEquals("Your Account Has Been Created!", driver.findElement(By.cssSelector("div#content > h1")).getText());
 
 
     }
 
+    @Disabled
     @Test
     void login() throws InterruptedException {
 
-        driver.get("http://192.168.8.129/");
+        Registration page = new Registration(driver);
+        Login loginpage = new Login(driver);
 
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        loginpage.clickMyAccount();
+        loginpage.clickLogin();
 
-        driver.findElement(By.cssSelector(".list-inline-item:nth-of-type(2) .d-md-inline")).click();
-        driver.findElement(By.linkText("Login")).click();
+        loginpage.setLoginEmail("tomas3@gmail.com");
+        loginpage.setLoginPassWord("Tomas");
+        loginpage.clickToLogin();
 
-        driver.findElement(By.id("input-email")).sendKeys("tomas3@gmail.com");
-        driver.findElement(By.id("input-password")).sendKeys("Tomas");
-        driver.findElement(By.cssSelector("[action] .btn-primary")).click();
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.titleIs("My Account"));
@@ -85,7 +98,9 @@ public class Testing {
         assertEquals("My Account", driver.findElement(By.cssSelector("#content h2:nth-of-type(1)")).getText());
 
         driver.findElement(By.linkText("Edit your account information")).click();
+
         Thread.sleep(2000);
+
         assertEquals("Tomas", driver.findElement(By.id("input-firstname")).getAttribute("value"));
         assertEquals("Bambliauskas", driver.findElement(By.id("input-lastname")).getAttribute("value"));
         assertEquals("tomas3@gmail.com", driver.findElement(By.id("input-email")).getAttribute("value"));
@@ -101,8 +116,8 @@ public class Testing {
         driver.findElement(By.cssSelector(".list-inline-item:nth-of-type(2) .d-md-inline")).click();
         driver.findElement(By.linkText("Login")).click();
 
-        driver.findElement(By.id("input-email")).sendKeys("tomas3@gmail.com");
-        driver.findElement(By.id("input-password")).sendKeys("Tomas");
+        page.setEmail("tomas3@gmail.com");
+        page.setPassword("Tomas");
         driver.findElement(By.cssSelector("[action] .btn-primary")).click();
 
         driver.findElement(By.linkText("Edit your account information")).click();
